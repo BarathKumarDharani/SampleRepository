f0efdf19ced3ddae7c47399941da1127dbb08f79
BarathKumarDharani <barathkumardharani30@gmail.com>
2020-06-03 19:57:00 +0530

    Congrats changes commited

--- Hello.java
+++ Hello.java
@@ -11,7 +11,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("cahnge Hello world"+i);
+			System.out.println("change Hello world" + i);
 		}
+
+		System.out.println("congrats");
 	}
 }
